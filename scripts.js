4b27af9e712b8f875118445793115c5c7f907a21
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-02-02 15:09:36 -0600

    Add ui functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
-//this will play a 5 round game of rock paper scissors against a computer opponent
-//determine number of rounds
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn");
+const results = document.querySelector("#results");
+const score = document.querySelector("#score");
+let playerWins = 0;
+let computerWins = 0;
+
 //get computers selection
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3);
@@ -13,44 +19,43 @@ function getComputerChoice() {
     };
 }
 //get users selection
-function getPlayerChoice() {
-    let selection = prompt("Enter rock, paper, or scissors");
-    return selection.toLowerCase();
-}
+rockBtn.addEventListener("click", () => playRound("rock", getComputerChoice()));
+paperBtn.addEventListener("click", () => playRound("paper", getComputerChoice()));
+scissorsBtn.addEventListener("click", () => playRound("scissors", getComputerChoice()));
+
 //compare selections and determine winner
 function playRound(playerSelection, computerSelection) {
     //losing scenarios
-    if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock") {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    if (playerSelection === "rock" && computerSelection === "paper" || 
+    playerSelection === "paper" && computerSelection === "scissors" || 
+    playerSelection === "scissors" && computerSelection === "rock") {
+        results.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
     //winning scenarios
-    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors" || 
+    playerSelection === "paper" && computerSelection === "rock" || 
+    playerSelection === "scissors" && computerSelection === "paper") {
+        results.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection === computerSelection) {
-        return `Draw, ${playerSelection} and ${computerSelection} are equal`;
+        results.textContent = `Draw, ${playerSelection} and ${computerSelection} are equal`;
     }
+    getScore();
 }
 
-function game() {
-    let computerWins = 0;
-    let playerWins = 0;
-    //restart game until 5 rounds have been played
-    for (let numberOfGames = 5; numberOfGames > 0; numberOfGames--) {
-        let result = playRound(getPlayerChoice(), getComputerChoice());
-        // track wins for best of 5 winner
-        if (result.includes("lose")) {
-            computerWins += 1;
-        } else if (result.includes("win")) {
-            playerWins += 1;
-        }
-        //display indivdual game result
-        console.log(result);
+function getScore() {
+    //reset score after a winner is determined
+    if (playerWins === 5 || computerWins === 5) {
+        playerWins = 0;
+        computerWins = 0;
     }
-    //determine best of 5 winner
-    if (computerWins > playerWins) {
-        console.log("You lost the best of 5 rounds");
-    } else if (playerWins > computerWins) {
-        console.log("You won the best of 5 rounds!");
-    } else console.log("Best of 5 was a draw");
-}
-game();
-
+    if (results.textContent.includes("win")) {
+        playerWins += 1;
+    } else if (results.textContent.includes("lose")) {
+        computerWins += 1;
+    }
+    score.textContent = `Player Score: ${playerWins}     Computer Score: ${computerWins}`;
+    if (playerWins === 5) {
+        score.innerHTML += "<br>You scored 5 points! Winner!";
+    } else if (computerWins === 5) {
+        score.innerHTML += "<br>Computer scored 5 points, you lose";
+    }
+};
